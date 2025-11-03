9ded3ddfe8a145a5d2412380f8c44a0912bad878
jjetta <johnjoshetta@gmail.com>
2025-11-03 11:38:38 -0600

    removed clear_session script, it only is needed when creating a new session

--- src/backend/clear_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/helpers/api_helpers.php';
-
-$username = getenv('API_USER');
-$password = getenv('API_PASS');
-
-$data = "username=$username&password=$password";
-api_call('clear_session', $data);
